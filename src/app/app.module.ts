d33b33aa3606c9a215e18cac8fca085ceb9af16a
root <root@localhost.localdomain>
2019-09-29 21:20:01 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { DatePipe } from './date.pipe';
+import { HightlightDirective } from './hightlight.directive';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    DatePipe,
+    HightlightDirective,
+    RepoComponent,
+    UserComponent,
+    NavbarComponent,
+    FormComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
