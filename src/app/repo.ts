e2951a2454cb78d923891fa8a00ad54f85461688
root <root@localhost.localdomain>
2019-09-30 04:41:16 -0400

    update the repo constructor

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,4 +1,4 @@
 export class Repo {
-    constructor(public name: string, public  html_url: string, public descriptiopn: string, created_at: Date,
+    constructor(public name: string, public  html_url: string, public description: string, created_at: Date,
         language: string) {}
 }
